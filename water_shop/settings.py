34f7813b35cccf86d4927f2a389b2e6c463f93f6
Umar Kholdarbekov <international-2014@mail.ru>
2023-02-09 11:45:41 +0500

    change DEBUG setting to False

--- water_shop/settings.py
+++ water_shop/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-nj9_gy_x=au(ms&9)9ypfwefe&xb9qz!20v%o7@cb1_%rgi7p4"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
